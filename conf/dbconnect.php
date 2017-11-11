fce8d1b62dd651e7bd0a65d88f2bddd1ebd2fb8d
Luke Tainton <luke@taintonfamily.co.uk>
2017-11-11 13:19:34 +0000

    Adding config files

---
+++ conf/dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+
+//SET VARIABLES
+$dbuser = "";
+$dbpass = "";
+$dbdatabase = "";
+$dbserver = "";
+
+//CONNECT TO DB
+$conn = mysqli_connect($dbserver, $dbuser, $dbpass, $dbdatabase);
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="./styles/style.css">
+        <title="Login ~ Citation Needed"></title>
+
+    </head>
+</html>
