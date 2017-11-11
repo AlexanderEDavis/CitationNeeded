e91f681eff58058c03fa57ca896d771d6e09bba6
Alexander Davis <AlexanderEDavis@users.noreply.github.com>
2017-11-11 14:30:16 +0000

    Rename Account and Config

---
+++ config.php
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDKRI0k99aX9JYEzdz8hQTfRuqbAIKQaiU",
+    authDomain: "astonhack17-citationnneeded.firebaseapp.com",
+    databaseURL: "https://astonhack17-citationnneeded.firebaseio.com",
+    projectId: "astonhack17-citationnneeded",
+    storageBucket: "astonhack17-citationnneeded.appspot.com",
+    messagingSenderId: "517295175221"
+  };
+  firebase.initializeApp(config);
+</script>
