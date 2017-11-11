d02822b55e835426c04d63b3dfbdb176e98efe31
Alexander Davis <AlexanderEDavis@users.noreply.github.com>
2017-11-11 20:36:13 +0000

    Undo Work

--- account/register.php
+++ account/register.php
@@ -49,7 +49,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
   -------------------------------------------------------------*/
   $insert="INSERT INTO users (email, salt, password) VALUES ('$username','$user_salt','$hashed_pwd')";
     mysqli_query($conn, $insert) or die("$insert".mysqli_error($conn));
-    header('Location:login');
 }
  ?>
 
