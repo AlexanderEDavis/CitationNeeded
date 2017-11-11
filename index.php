501c01857c98a568d0ccc12708dd985f1c67f4a9
Alexander Davis <AlexanderEDavis@users.noreply.github.com>
2017-11-11 17:23:08 +0000

    Sescheck and Index update

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+//include both the dbconnect and sescheck files
+require('./conf/dbconnect.php');
+require('./conf/sescheck.php');
+?>
+
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0; url=./home" />
+    </head>
+</html>
