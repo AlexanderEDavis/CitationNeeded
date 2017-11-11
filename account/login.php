572e61ad5e0c193ec5c4b84fdd4d43a3ef8cf07a
Alexander Davis <AlexanderEDavis@users.noreply.github.com>
2017-11-11 17:07:16 +0000

    Final changes before reset

--- account/login.php
+++ account/login.php
@@ -3,23 +3,11 @@
         <link rel="stylesheet" href="../assets/style/style.css">
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
         <title>Login ~ Citation Needed</title>
-        <script src="../scripts/require.js">
-        <script type="text/javascript">
-        require('../scripts/config.js')
-        session_start();
-
-        date_default_timezone_set('Europe/London');
-
-        if($_SERVER["REQUEST_METHOD"] == "POST") {
-           // username and password sent from form
-           firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-             // Handle Errors here.
-             var errorCode = error.code;
-             var errorMessage = error.message;
-             // ...
-           });
-         }
-        </script>
+        <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase-app.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase-auth.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase-database.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase-firestore.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase-messaging.js"></script>
     </head>
     <body>
             <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -74,7 +62,6 @@
                                 <input class="mdl-textfield__input" type="password" id="password" name="password">
                                 <label class="mdl-textfield__label" for="password">Password</label>
                               </div>
-
                               <!-- <div class="mdl-textfield is-invalid mdl-js-textfield mdl-textfield--floating-label">
                                 <input class="mdl-textfield__input" type="text" id="email" name="email">
                                 <label class="mdl-textfield__label" for="email">Email Address</label>
@@ -90,7 +77,7 @@
 
                             <!-- ACTIONS CARD START -->
                             <div class="mdl-card__actions mdl-card--border">
-                              <input class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary" type="submit" value="Log In">
+                              <input class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary" type="submit" value="Log In" onclick="">
                             </div>
                             <!-- ACTIONS CARD END -->
 
@@ -100,5 +87,45 @@
                       </div>
                     </main>
                   </div>
+
+                  <script type="text/javascript">
+
+                    // Initialize Firebase
+                    var config = {
+                      apiKey: "AIzaSyDKRI0k99aX9JYEzdz8hQTfRuqbAIKQaiU",
+                      authDomain: "astonhack17-citationnneeded.firebaseapp.com",
+                      databaseURL: "https://astonhack17-citationnneeded.firebaseio.com",
+                      projectId: "astonhack17-citationnneeded",
+                      storageBucket: "astonhack17-citationnneeded.appspot.com",
+                      messagingSenderId: "517295175221"
+                    };
+
+                    firebase.initializeApp(config);
+                    // session_start();
+
+                  // date_default_timezone_set('Europe/London');
+
+                  var logUserIn = function() {
+
+                    var user1 = document.getElementById("email");
+                    var username = user1.toString();
+                    var pass1 = document.getElementById("password");
+                    var password = pass1.toString();
+
+                     // username and password sent from form
+                     firebase.auth().signInWithEmailAndPassword("pandelis@itdoesntmatter.com", "password").catch(function(error) {
+                       // Handle Errors here.
+                       var errorCode = error.code;
+                       var errorMessage = error.message;
+                       console.log(errorMessage);
+                       // ...
+                     });
+                   }
+
+                   <?php if($_SERVER["REQUEST_METHOD"] == "POST")
+                   logUserIn()
+                     ?>
+                   <?php; ?>
+                  </script>
     </body>
 </html>
