67801f19796d582d368606b57ac5dc61fd21cc8e
Luke Tainton <luke@taintonfamily.co.uk>
2017-11-11 14:56:05 +0000

    Change Settings page

--- account/settings.php
+++ account/settings.php
@@ -2,37 +2,34 @@
     <head>
         <link rel="stylesheet" href="../assets/style/style.css">
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-        <title>Login ~ Citation Needed</title>
+        <title>Account Settings ~ Citation Needed</title>
     </head>
     <body>
-            <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-                    <header class="mdl-layout__header">
-                      <div class="mdl-layout__header-row">
-                        <!-- Title -->
-                        <span class="mdl-layout-title">Welcome to Citation Needed</span>
-                        <!-- Add spacer, to align navigation to the right -->
-                        <div class="mdl-layout-spacer"></div>
-                        <!-- Navigation. We hide it in small screens. -->
-                        <!-- <nav class="mdl-navigation mdl-layout--large-screen-only">
-                          <a class="mdl-navigation__link" href="">Link</a>
-                          <a class="mdl-navigation__link" href="">Link</a>
-                          <a class="mdl-navigation__link" href="">Link</a>
-                          <a class="mdl-navigation__link" href="">Link</a>
-                        </nav> -->
-                      </div>
-                    </header>
-                    <!-- <div class="mdl-layout__drawer">
-                      <span class="mdl-layout-title">Title</span>
-                      <nav class="mdl-navigation">
-                        <a class="mdl-navigation__link" href="">Link</a>
-                        <a class="mdl-navigation__link" href="">Link</a>
-                        <a class="mdl-navigation__link" href="">Link</a>
-                        <a class="mdl-navigation__link" href="">Link</a>
-                      </nav>
-                    </div> -->
-                    <main class="mdl-layout__content">
-                      <div class="page-content"><!-- Your content goes here --></div>
-                    </main>
-                  </div>
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+            <header class="mdl-layout__header">
+                <div class="mdl-layout__header-row">
+                    <div class="mdl-layout-spacer"></div>
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+                        <label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer-exp">
+                            <i class="material-icons">search</i>
+                        </label>
+                        <div class="mdl-textfield__expandable-holder">
+                            <input class="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp">
+                        </div>
+                    </div>
+                </div>
+            </header>
+        <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title">Citation Needed</span>
+            <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="../home">Home</a>
+                <a class="mdl-navigation__link" href="../account/settings">Account Settings</a>
+                <a class="mdl-navigation__link" href="../account/logout">Logout</a>
+            </nav>
+        </div>
+            <main class="mdl-layout__content">
+                <div class="page-content"><!-- Your content goes here --></div>
+            </main>
+        </div>
     </body>
 </html>
