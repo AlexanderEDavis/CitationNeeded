655c1509f0b745faf6579bb8ba3eac93ba6e4baa
Luke Tainton <luke@taintonfamily.co.uk>
2017-11-12 10:00:51 +0000

    Edit Bib page

--- home/editbib.php
+++ home/editbib.php
@@ -7,7 +7,7 @@ require('../conf/sescheck.php');
 $email = $_SESSION['email'];
 $bid = $_GET["id"];
 
-$viewQry = "SELECT * FROM bibliographies WHERE bid = $bid";
+$viewQry = "SELECT * FROM bibliographies WHERE bid = '$bid'";
 $bibSql_getBib = mysqli_query($conn,$viewQry) or die("You do not have permissions to read this bibliography.");
 $row = mysqli_fetch_assoc($bibSql_getBib);
 
@@ -25,14 +25,14 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     <head>
         <link rel="stylesheet" href="../assets/style/style.css">
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-        <title>Editing <?php echo($row['bibname']); ?> ~ Citation Needed</title>
+        <title>Editing <?php echo($row['name']); ?> ~ Citation Needed</title>
     </head>
     <body>
 
         <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
             <header class="mdl-layout__header">
                 <div class="mdl-layout__header-row">
-                    <span class="mdl-layout-title">Edit Bibliography: <em> <?php echo($row['bibname']); ?> </em></span>
+                    <span class="mdl-layout-title">Edit Bibliography: <em> <?php echo($row['name']); ?> </em></span>
                     <div class="mdl-layout-spacer"></div>
                     <nav class="mdl-navigation mdl-layout--large-screen-only">
                           <!-- <a class="mdl-navigation__link" href="#">Create Bibliography</a> -->
@@ -70,7 +70,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
           <!-- MAIN CARD START -->
         <div class="mdl-card__supporting-text">
             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-              <input class="mdl-textfield__input" type="text" id="name" name="bibname" value="<?php echo($row['bibname']); ?>"></input>
+              <input class="mdl-textfield__input" type="text" id="name" name="bibname" value="<?php echo($row['name']); ?>"></input>
               <label class="mdl-textfield__label" for="authors">Name</label>
             </div>
 
