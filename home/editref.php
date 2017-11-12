1d18157a54ebd54ca000cf531a0203a8855ff6c2
Luke Tainton <luke@taintonfamily.co.uk>
2017-11-12 05:59:56 +0000

    Create edit file, populate form

--- home/editref.php
+++ home/editref.php
@@ -8,6 +8,10 @@ $email = $_SESSION['email'];
 $bid = $_GET["id"];
 $rid = $_GET["rid"];
 
+$viewQry = "SELECT * FROM reference WHERE rid = $rid";
+$refSql_getRef = mysqli_query($conn,$viewQry) or die("You do not have permissions to read this bibliography.");
+$row = mysqli_fetch_assoc($refSql_getRef);
+
 // $refQry_newRef = "INSERT INTO reference (bibliography, reftype, authors) VALUES ('$bid', '$reftype', '$authors')";
 // $refSql_newRef = mysqli_query($conn,$refQry_newRef) or die("Could not create reference. ".mysqli_error($conn));
 
@@ -25,14 +29,14 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     <head>
         <link rel="stylesheet" href="../assets/style/style.css">
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-        <title>Create Reference ~ Citation Needed</title>
+        <title>Editing <?php echo($row['refname']); ?> ~ Citation Needed</title>
     </head>
     <body>
 
         <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
             <header class="mdl-layout__header">
                 <div class="mdl-layout__header-row">
-                    <span class="mdl-layout-title">Create New Reference</span>
+                    <span class="mdl-layout-title">Edit Reference: <em> <?php echo($row['refname']); ?> </em></span>
                     <div class="mdl-layout-spacer"></div>
                     <nav class="mdl-navigation mdl-layout--large-screen-only">
                           <!-- <a class="mdl-navigation__link" href="#">Create Bibliography</a> -->
@@ -61,17 +65,27 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
                     <!-- Get User's Bibs -->
                     <!-- Create Delete buttons on each card -->
 
-                    <!-- LOGIN CARD START -->
-        <div id="cardNewRef" class="mdl-card mdl-shadow--4dp">
+                   <!-- LOGIN CARD START -->
+        <div id="cardEditRef" class="mdl-card mdl-shadow--4dp">
 
         <!-- LOGIN FORM START -->
-        <form method="POST" action="" id="newRef">
+        <form method="POST" action="" id="editRef">
 
           <!-- MAIN CARD START -->
         <div class="mdl-card__supporting-text">
             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input class="mdl-textfield__input" type="text" id="name" name="refName"></input>
+              <label class="mdl-textfield__label" for="authors">Name</label>
+            </div>
+
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
               <input class="mdl-textfield__input" type="text" id="name" name="authors"></input>
               <label class="mdl-textfield__label" for="authors">Author(s)</label>
+            </div>
+
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input class="mdl-textfield__input" type="text" id="name" name="year"></input>
+              <label class="mdl-textfield__label" for="year">Year</label>
             </div>
 
             <!-- Select with arrow-->
@@ -85,6 +99,13 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
             </ul>
           </select>
         </div>
+
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input class="mdl-textfield__input" type="text" id="name" name="url"></input>
+              <label class="mdl-textfield__label" for="year">URL</label>
+        </div>
+
+        
         </div>
           <!-- MAIN CARD END -->
 
