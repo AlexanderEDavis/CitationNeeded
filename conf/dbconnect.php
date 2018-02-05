464f90a370278e91d214e87effd9e353cdf1f531
Luke Tainton <luke@taintonfamily.co.uk>
2018-02-05 17:41:39 +0000

    Added die to the MySQL connection

--- conf/dbconnect.php
+++ conf/dbconnect.php
@@ -5,5 +5,5 @@ $dbpass = "";
 $dbdatabase = "";
 $dbserver = "";
 //CONNECT TO DB
-$conn = mysqli_connect($dbserver, $dbuser, $dbpass, $dbdatabase);
+$conn = mysqli_connect($dbserver, $dbuser, $dbpass, $dbdatabase) or die("There was an error connecting to the database.");
 ?>
